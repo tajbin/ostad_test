774225b388f347656f70372edf42e560f9456c37
tajbin <tajbin2013@gmail.com>
2024-03-16 00:08:31 +0600

    Initial commit

---
+++ bin/ostad_test.dart
@@ -0,0 +1,31 @@
+void main() {
+  List<Map<String, dynamic>> fruits = [
+    {"name": "Apple", "color": "Red", "price": 2.5},
+    {"name": "Banana", "color": "Yellow", "price": 1.0},
+    {"name": "Grapes", "color": "Purple", "price": 3.0},
+  ];
+
+  for (var details in fruits) {
+    displayFruitsDetails(
+      name: details['name'].toString(),
+      color: details['color'].toString(),
+      price: details['price'],
+    );
+  }
+  print("Fruit Details After Applying 10% Discount:");
+  for (var afterDiscount in fruits) {
+    applyPriceDiscount(
+      name: afterDiscount['name'].toString(),
+      color: afterDiscount['color'].toString(),
+      price: afterDiscount['price'],
+    );
+  }
+}
+
+void displayFruitsDetails({required String name, required String color, required double price}) {
+  print("Name: $name, Color: $color, Price: \$${price}");
+}
+
+void applyPriceDiscount({required String name, required String color, required double price}){
+  print("Name: $name, Color: $color, Price: \$${price-(price*10/100)}");
+}
---
+++ test/ostad_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostad_test/ostad_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
